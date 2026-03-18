6ffd30aa3b6ed3344c1c62486db7cb7cfae8b014
Jehyuk <cjhyuck213@gmail.com>
2026-03-19 02:58:09 +0800

    Rename state snapshot registered members field

--- src/interface/channel/types.ts
+++ src/interface/channel/types.ts
@@ -1,6 +1,6 @@
 export type StateSnapshot = {
   stateRoots: string[];
-  registeredKeys: {storageAddress: string, members: {key: string, value: string}[]}[];
+  registeredMembers: {storageAddress: string, members: {key: string, value: string}[]}[];
   channelId: number;
 }
 
