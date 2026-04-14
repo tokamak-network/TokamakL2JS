2856dfaafc757405925a8dc79262ccdb76e24c04
Jehyuk <cjhyuck213@gmail.com>
2026-04-14 17:33:55 +0800

    Set MT depth to 30 and bump version to 0.1.2

--- src/interface/params/stateManager.ts
+++ src/interface/params/stateManager.ts
@@ -6,5 +6,5 @@ export const NULL_STORAGE_KEY = jubjub.Point.Fp.ORDER - 1n;
 export const NULL_LEAF = 0n;
 
 // Params to feed QAP Compiler 
-export const MT_DEPTH = 36;
+export const MT_DEPTH = 30;
 export const MAX_MT_LEAVES = POSEIDON_INPUTS ** MT_DEPTH;
